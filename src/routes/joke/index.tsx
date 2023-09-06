9f66f1907b655cab2253ab62ea8358aab73dc4fc
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-09-06 19:41:15 +0200

    Post data to the server

--- src/routes/joke/index.tsx
+++ src/routes/joke/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { routeLoader$, Form, routeAction$ } from '@builder.io/qwik-city';
  
 export const useDadJoke = routeLoader$(async () => {
   const response = await fetch('https://icanhazdadjoke.com/', {
@@ -12,12 +12,26 @@ export const useDadJoke = routeLoader$(async () => {
   };
 });
  
+export const useJokeVoteAction = routeAction$((props) => {
+  console.log('VOTE', props);
+});
+ 
 export default component$(() => {
   // Calling our `useDadJoke` hook, will return a reactive signal to the loaded data.
   const dadJokeSignal = useDadJoke();
+  const favoriteJokeAction = useJokeVoteAction();
   return (
     <section class="section bright">
       <p>{dadJokeSignal.value.joke}</p>
+      <Form action={favoriteJokeAction}>
+        <input type="hidden" name="jokeID" value={dadJokeSignal.value.id} />
+        <button name="vote" value="up">
+          👍
+        </button>
+        <button name="vote" value="down">
+          👎
+        </button>
+      </Form>
     </section>
   );
 });
